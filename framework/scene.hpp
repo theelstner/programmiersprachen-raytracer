19d0a88c8f5ac483216457559a2fd9cf0d7982f8
Theresa Elstner <theresa.elstner@gmail.com>
2015-08-09 17:21:37 +0200

    add vector lights

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -5,13 +5,13 @@
 #include <material.hpp>
 #include <sphere.hpp>
 #include <box.hpp>
-//#include <light.hpp>;
-//#include <camera.hpp>;
+#include <light.hpp>
+//#include <camera.hpp>
 
 struct Scene
 {
 	std::vector<Material> materials;
-	//std::vector<Light> lights;
+	std::vector<Light> lights;
 	std::vector<Sphere> sphere;
 	std::vector<Box> box;
 	//Camera camera;
